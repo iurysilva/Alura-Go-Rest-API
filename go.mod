66f52e76d53c40760ec63445ee6a8bc2f01a977a
isilva <iury.silva@w3as.com.br>
2023-08-31 10:30:08 -0300

    Revert "feat: adiciona gorilla mux para endpoints"
    This reverts commit a29775ce554489443f921c247077c9fc11d047fa.

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/iurysilva/Alura-Go-Rest-API
 
 go 1.21.0
-
-require github.com/gorilla/mux v1.8.0
--- main.go
+++ main.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/iurysilva/Alura-Go-Rest-API/routes"
+	"log"
+	"net/http"
 )
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Home Page")
+}
+
+func HandleRequest() {
+	http.HandleFunc("/", Home)
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
 
 func main() {
 	fmt.Println("Iniciando o servidor Rest com Go")
-	routes.HandleRequest()
+	HandleRequest()
 }
