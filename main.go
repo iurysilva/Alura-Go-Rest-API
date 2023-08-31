d9e4c98d7337b26c0d4a3392d5d590fb65fe3c6c
isilva <iury.silva@w3as.com.br>
2023-08-31 11:55:26 -0300

    Implementa conexão com o banco de dados usando GORM

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 
+	"github.com/iurysilva/Alura-Go-Rest-API/database"
 	"github.com/iurysilva/Alura-Go-Rest-API/routes"
 )
 
 func main() {
+	database.ConnectWithDatabase()
 	fmt.Println("Iniciando o servidor Rest com Go")
 	routes.HandleRequest()
 }
